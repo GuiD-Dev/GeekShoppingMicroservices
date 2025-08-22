f4e167520249a97184944daff61dd9bb854895f3
Guilherme <guilhermed.dev@gmail.com>
2025-08-22 00:47:11 -0300

    fix(ProductAPI/MySQLContext): fix ImageUrl value in some product on initializing db

--- ProductAPI/DBContext/MySQLContext.cs
+++ ProductAPI/DBContext/MySQLContext.cs
@@ -116,7 +116,7 @@ public class MySQLContext : DbContext
                 Name = "Goku Phases T-Shirt",
                 Price = 59.99m,
                 Description = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.<br/>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.<br/>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
-                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/13_dragon_ball.jpg",
+                ImageUrl = "https://github.com/leandrocgsi/erudio-microservices-dotnet6/blob/main/ShoppingImages/13_dragon_ball.jpg?raw=true",
                 Category = "T-shirt"
             });
     }
